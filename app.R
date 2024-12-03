1b4c5609671ca19b743087779dbd08e5532a702b
Yunyi Ren <yyren@ucdavis.edu>
2024-12-03 09:14:43 -0800

    delete comments

--- app.R
+++ app.R
@@ -1,26 +1,3 @@
-# library(shiny)
-# library(shinydashboard)
-#
-# # Source the module files
-# source("R/mod_starwars_ui.R")
-# source("R/mod_starwars_server.R")
-#
-# # Define the UI
-# app_ui <- function() {
-#     fluidPage(
-#         mod_starwars_ui("starwars")
-#     )
-# }
-#
-# # Define the server
-# app_server <- function(input, output, session) {
-#     callModule(mod_starwars_server, "starwars")
-# }
-#
-# # Run the Shiny App
-# shinyApp(ui = app_ui, server = app_server)
-
-
 1# Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
